93d9345c60916dd38c728e122b82cd3a0110a87a
declanmichael7 <declanmichael7@gmail.com>
2021-01-04 01:15:00 -0500

    Added Seeding function
    Added the ability to copy a game board from a another generation. This can be a stand in for allowing people to play the same game on different screens for now

--- Codenames.js
+++ Codenames.js
@@ -15,14 +15,12 @@ function randomStart(){
 	document.getElementById("startPage").style.display = "none";
 	document.getElementById("gamePage").style.display = "initial";
 	turnColor = Math.floor(Math.random() * 2);
-	console.log(turnColor);
 	gameCode += turnColor;
 	while(i<=25){
 		word = getWord();
 		document.getElementById(i).innerHTML=currentList[word];
 		i++;
 	}
-	console.log("test");
 	getAlign();
 	if(turnColor == 0){
 		document.getElementById("currentTurn").innerHTML = "Current turn: Red";
@@ -34,18 +32,15 @@ function randomStart(){
 	}
 }
 function getAlign(){
-	console.log("test2");
 	var num;
 	var usedNum = [];
 	if(turnColor==0){
-		console.log("test3");
 		while(redWords.length<9){
 			num = Math.floor(Math.random() * 25)+1;
 			if(!usedNum.includes(num)){
 				usedNum.push(num);
 				redWords.push(num);
 			}
-			console.log(num);
 		}
 		while(blueWords.length<8){
 			num = Math.floor(Math.random() * 25)+1;
@@ -53,18 +48,15 @@ function getAlign(){
 				usedNum.push(num);
 				blueWords.push(num);
 			}
-			console.log(num);
 		}
 	}
 	else{
-		console.log("test3");
 		while(redWords.length<8){
 			num = Math.floor(Math.random() * 25)+1;
 			if(!usedNum.includes(num)){
 				usedNum.push(num);
 				redWords.push(num);
 			}
-			console.log(num);
 		}
 		while(blueWords.length<9){
 			num = Math.floor(Math.random() * 25)+1;
@@ -72,11 +64,8 @@ function getAlign(){
 				usedNum.push(num);
 				blueWords.push(num);
 			}
-			console.log(num);
 		}
 	}
-	console.log(redWords);
-	console.log(blueWords);
 	while(neutralWords.length<7){
 		num = Math.floor(Math.random() * 25)+1;
 		if(!usedNum.includes(num)){
@@ -84,9 +73,7 @@ function getAlign(){
 			neutralWords.push(num);
 		}
 	}
-	console.log(neutralWords);
 	i=1;
-	console.log(deathWord);
 	while(i<=25 && deathWord==-1){
 		if(!usedNum.includes(i)){
 			deathWord = i;
@@ -95,7 +82,6 @@ function getAlign(){
 			i++;
 		}
 	}
-	console.log(deathWord);
 		
 }
 function getWord(){
@@ -105,6 +91,7 @@ function getWord(){
 		return getWord();
 	}
 	else{
+		words.push(word);
 		return word;
 	}
 }
@@ -112,12 +99,11 @@ function seedStart(){
 	currentList=defaultList;
 	gameCode = prompt("Enter the game code");
 	var code = gameCode.split(" ");
-	console.log(code);
 	document.getElementById("startPage").style.display = "none";
 	document.getElementById("gamePage").style.display = "initial";
 	turnColor = code[0];
 	i=1;
-	while(i<=26){
+	while(i<=25){
 		if(code[i].charAt(0)==0){
 			redWords.push(i);
 			document.getElementById(i).innerHTML = currentList[parseInt(code[i].substr(1),16)];
@@ -134,14 +120,30 @@ function seedStart(){
 			deathWord = i;
 			document.getElementById(i).innerHTML = currentList[parseInt(code[i].substr(1),16)];
 		}
-		console.log(i);
-		console.log(currentList[parseInt(code[i].substr(1),16)]);
 		i++;
 	}
 }
 function getCode(){
+	i=1;
+	while(i<=25){
+		if(deathWord == i){
+			gameCode += " " + 3;
+		}
+		else if(neutralWords.includes(i)){
+			gameCode += " " + 2;
+		}
+		else if(blueWords.includes(i)){
+			gameCode += " " + 1;
+		}
+		else{
+			gameCode += " " + 0;
+		}
+		gameCode += words[i-1].toString(16);
+		i++;
+	}
 	navigator.clipboard.writeText(gameCode).then(function() {
-			alert("Game Code copied to clipboard");
+		
+		alert("Game Code copied to clipboard");
 		}, function() {
 			alert("error");
 			console.log(gameCode);
